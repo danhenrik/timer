893c82949408b6612dab409adf49b9578d61b5b0
danhenrik <danihtoledo22@gmail.com>
2021-10-05 00:12:51 -0300

    reorganization & reset fix

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// TODO: Essa parada de reset no final ta bugando tudo (com o enter) possível solução permitir enter apenas se tivermos algum valor nas entradas
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let go = false;
 let digitsCounter = 2;
 let timePointer = 0;
-let SeparatorPointer = 0;
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let separatorPointer = 0;
+let interv;
 
 const timer = [
   document.getElementById('seconds-1'),
@@ -19,49 +22,45 @@ const separator = [
   document.getElementById('H-specifier'),
 ];
 
-const goButton = document.getElementById('goButton');
-goButton.addEventListener('click', (event) => {
-  init();
-});
+// Buttons funcionalities
+document.getElementById('goButton').onclick = init;
+document.getElementById('resetButton').onclick = reset;
 
-const resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', () => {
-  reset();
-});
+function initCountdown(timer) {
+  interv = setInterval(() => {
+    let hours = `${timer[5].textContent}${timer[4].textContent}`;
+    let minutes = `${timer[3].textContent}${timer[2].textContent}`;
+    let seconds = `${timer[1].textContent}${timer[0].textContent}`;
 
-function reset() {
-  timer.forEach((tm) => (tm.textContent = ''));
-  separator.forEach((sep) => (sep.hidden = true));
-  go = false;
-  digitsCounter = 2;
-  timePointer = 0;
-  SeparatorPointer = 0;
-}
-
-function init() {
-  go = true;
-  const convertedTimer = convertTimer(timer);
-  initCountdown(convertedTimer);
-}
-
-function initCountdown(timerArr) {
-  const interv = setInterval(() => {
-    let hours = `${timerArr[5].textContent}${timerArr[4].textContent}`;
-    let minutes = `${timerArr[3].textContent}${timerArr[2].textContent}`;
-    let seconds = `${timerArr[1].textContent}${timerArr[0].textContent}`;
     seconds--;
+
     if (seconds == -1) {
       minutes--;
       seconds = 59;
     }
-    // } else if (seconds == -1 && hours > 0) {
-    // hours--;
-    // minutes = 59;
-    // seconds = 59;
-    // }
+
     if (minutes == -1) {
       hours--;
       minutes = 59;
+    }
+
+    if (timePointer == 5 && minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (timePointer == 3 && seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    if (minutes == 0) {
+      console.log('entrou no if');
+      timer[3].textContent = '';
+      timer[2].textContent = '';
+      separator[1].hidden = true;
+    }
+    if (hours == 0) {
+      timer[5].textContent = '';
+      timer[4].textContent = '';
+      separator[2].hidden = true;
     }
     // TODO: Reset is triggering alarm, have a look at this.
     // TODO: Any key or mouse click triggers voice
@@ -84,31 +83,31 @@ function initCountdown(timerArr) {
       hours = hours?.toString();
 
       if (hours) {
-        timerArr[5].textContent = hours[0] || 0;
-        timerArr[4].textContent = hours[1];
+        timer[5].textContent = hours[0];
+        timer[4].textContent = hours[1];
       }
       if (minutes) {
-        timerArr[3].textContent = minutes[0] || 0;
-        timerArr[2].textContent = minutes[1];
+        timer[3].textContent = minutes[0];
+        timer[2].textContent = minutes[1];
       }
       if (seconds) {
-        timerArr[1].textContent = seconds[0] || 0;
-        timerArr[0].textContent = seconds[1];
+        timer[1].textContent = seconds[0];
+        timer[0].textContent = seconds[1];
       }
     }
   }, 1000);
 }
 
-function convertTimer(timerArr) {
-  let hours = `${timerArr[5].textContent}${timerArr[4].textContent}` || '';
-  let minutes = `${timerArr[3].textContent}${timerArr[2].textContent}` || '';
-  let seconds = `${timerArr[1].textContent}${timerArr[0].textContent}` || '';
+function convertTimer(timer) {
+  let hours = `${timer[5].textContent}${timer[4].textContent}` || '';
+  let minutes = `${timer[3].textContent}${timer[2].textContent}` || '';
+  let seconds = `${timer[1].textContent}${timer[0].textContent}` || '';
 
   if (seconds > 60) {
     minutes++;
     if (minutes == 1) {
       separator[1].hidden = false;
-      SeparatorPointer = 2;
+      separatorPointer = 2;
       digitsCounter = 1;
       timePointer++;
     }
@@ -118,7 +117,7 @@ function convertTimer(timerArr) {
     hours++;
     if (hours == 1) {
       separator[2].hidden = false;
-      SeparatorPointer = 3;
+      separatorPointer = 3;
       digitsCounter = 1;
       timePointer++;
     }
@@ -128,92 +127,108 @@ function convertTimer(timerArr) {
     //* Need to be a max of 2 digits long
     hours = 99;
   }
+
   seconds = seconds.toString();
   minutes = minutes.toString();
   hours = hours.toString();
 
-  timerArr[5].textContent = hours[0];
-  timerArr[4].textContent = hours[1];
-  timerArr[3].textContent = minutes[0];
-  timerArr[2].textContent = minutes[1];
-  timerArr[1].textContent = seconds[0];
-  timerArr[0].textContent = seconds[1];
-  return timerArr;
+  timer[5].textContent = hours[0];
+  timer[4].textContent = hours[1];
+  timer[3].textContent = minutes[0];
+  timer[2].textContent = minutes[1];
+  timer[1].textContent = seconds[0];
+  timer[0].textContent = seconds[1];
 }
 
-document.addEventListener('keydown', (event) => {
-  // TODO: Refactor
-  if (numbers.includes(event.key) && timePointer < 6) {
-    if (digitsCounter == 2) {
-      separator[SeparatorPointer].hidden = false;
-      SeparatorPointer++;
-      digitsCounter = 0;
-    }
-    for (let i = timePointer; i > 0; i--) {
-      timer[i].textContent = timer[i - 1].textContent;
-    }
-    timer[0].textContent = event.key;
-    timePointer++;
-    digitsCounter++;
+function init() {
+  if (timer[0].textContent != '' && !go) {
+    go = true;
+    convertTimer(timer);
+    initCountdown(timer);
   }
+}
 
-  if (event.key == 'Backspace' && timer[0].textContent != '' && !go) {
-    for (let i = 0; i < timePointer; i++) {
-      if (timer[i + 1] == undefined) timer[i].textContent = '';
-      else timer[i].textContent = timer[i + 1].textContent;
-    }
-    if (digitsCounter == 1) {
-      separator[SeparatorPointer - 1].hidden = true;
-      digitsCounter = 2;
-      SeparatorPointer--;
-    } else {
-      digitsCounter--;
-    }
-    timePointer--;
-  }
+function reset() {
+  // TODO: Reset is transforming everything in the entry
+  clearInterval(interv);
+  timer.forEach((tm) => (tm.textContent = ''));
+  separator.forEach((sep) => (sep.hidden = true));
+  go = false;
+  digitsCounter = 2;
+  timePointer = 0;
+  separatorPointer = 0;
+}
 
-  if (event.key == 'Enter' && !go) {
-    init();
-  }
-
-  const synth = window.speechSynthesis;
-  if (event.key == ' ' && !synth.pending && go) {
-    let output = '';
-    if (timer[5].textContent || timer[4].textContent) {
-      if (timer[5].textContent == 0 && timer[4].textContent == 1) {
-        output += `${timer[4].textContent} hour,`;
-      } else {
-        output += `${timer[5].textContent}${timer[4].textContent} hours,`;
+function main() {
+  document.addEventListener('keydown', (event) => {
+    // TODO: Refactor
+    console.log(event.key);
+    if (numbers.includes(event.key) && timePointer < 6) {
+      console.log(
+        `[go] ${go}\n[digitsCounter] ${digitsCounter}\n[timePointer] ${timePointer}\n[separatorPointer] ${separatorPointer}`
+      );
+      if (digitsCounter == 2) {
+        separator[separatorPointer].hidden = false;
+        separatorPointer++;
+        digitsCounter = 0;
       }
-    }
-    if (timer[3].textContent || timer[2].textContent) {
-      if (timer[3].textContent == 0 && timer[2].textContent == 1) {
-        output += `${timer[2].textContent} minute and`;
-      } else {
-        output += `${timer[3].textContent}${timer[2].textContent} minutes and`;
+      for (let i = timePointer; i > 0; i--) {
+        timer[i].textContent = timer[i - 1].textContent;
       }
+      timer[0].textContent = event.key;
+      timePointer++;
+      digitsCounter++;
     }
 
-    if (timer[1].textContent == 0 && timer[0].textContent == 1) {
-      output += `${timer[0].textContent} second left.`;
-    } else {
-      output += `${timer[1].textContent}${timer[0].textContent} seconds left.`;
+    if (event.key == 'Backspace' && timer[0].textContent != '' && !go) {
+      for (let i = 0; i < timePointer; i++) {
+        if (timer[i + 1] == undefined) timer[i].textContent = '';
+        else timer[i].textContent = timer[i + 1].textContent;
+      }
+      if (digitsCounter == 1) {
+        separator[separatorPointer - 1].hidden = true;
+        digitsCounter = 2;
+        separatorPointer--;
+      } else {
+        digitsCounter--;
+      }
+      timePointer--;
     }
 
-    const speech = new SpeechSynthesisUtterance(output);
-    const voices = synth.getVoices();
-    speech.voice = voices[2];
-    synth.speak(speech);
-  }
+    if (event.key == 'Enter') {
+      init();
+    }
 
-  /* //Debug
-  console.log(
-    'digitsCounter',
-    digitsCounter,
-    'SeparatorPointer',
-    SeparatorPointer,
-    'timePointer',
-    timePointer
-  );
-  */
-});
+    const synth = window.speechSynthesis;
+    if (event.key == ' ' && !synth.pending && go) {
+      let output = '';
+      if (timer[5].textContent || timer[4].textContent) {
+        if (timer[5].textContent == 0 && timer[4].textContent == 1) {
+          output += `${timer[4].textContent} hour,`;
+        } else {
+          output += `${timer[5].textContent}${timer[4].textContent} hours,`;
+        }
+      }
+      if (timer[3].textContent || timer[2].textContent) {
+        if (timer[3].textContent == 0 && timer[2].textContent == 1) {
+          output += `${timer[2].textContent} minute and`;
+        } else {
+          output += `${timer[3].textContent}${timer[2].textContent} minutes and`;
+        }
+      }
+
+      if (timer[1].textContent == 0 && timer[0].textContent == 1) {
+        output += `${timer[0].textContent} second left.`;
+      } else {
+        output += `${timer[1].textContent}${timer[0].textContent} seconds left.`;
+      }
+
+      const speech = new SpeechSynthesisUtterance(output);
+      const voices = synth.getVoices();
+      speech.voice = voices[2];
+      synth.speak(speech);
+    }
+  });
+}
+
+document.onload = main();
